2d1539b3e2442807462df21c1ba5ba8672ffd1dc
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-03-15 22:29:44 +0500

    sdf

---
+++ Informatics/Funkcii/Theme 2. Rekyrsii/Yslovia 2/C. Fishki.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+void fill(int n);
+void remove(int n);
+
+void fill(int n) {
+    if (n == 1) {
+        cout << 1 << ' ';
+        return;
+    }
+    if (n >= 2) {
+        fill(n - 1);
+        cout << n << ' ';
+        remove(n - 1);
+    }
+}
+
+void remove(int n) {
+    if (n == 1) {
+        cout << -1 << ' ';
+        return;
+    }
+    if (n >= 2) {
+        fill(n - 1);
+        cout << -n << ' ';
+        remove(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = n; i >= 1; i--)
+        fill(i);
+    return 0;
+}
