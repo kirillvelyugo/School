58b13219bf2261ee9980a0d706d45a3d5be7c0c3
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-04-16 11:51:49 +0500

    dsf

---
+++ Ege/Задания/Алгоритмы/Решето Эратосфена/resheto Eratosfena.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+    vector <bool> nums(size + 1, 1);
+
+    for (int i = 2; i * i <= size; i++) {
+        if (nums[i]) {
+            for (int j = i * i; j <= size; j += i) {
+                nums[j] = 0;
+            }
+        }
+    }
+
+    for (int i = 2; i < nums.size(); i++) {
+        if (nums[i]) {
+            cout << i << ' ';
+        }
+    }
+
+    return 0;
+}
---
+++ Ege/Задания/Практика 15.04.2021/Решения/zadaniye 1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void countDel(int n, vector <int>& divs) {
+    for (int i = 1; i * i <= n; i++) {
+        if (i * i == n) {
+            divs.push_back(i);
+        }
+        else if (n % i == 0) {
+            divs.push_back(i);
+            divs.push_back(n / i);
+        }
+    }
+}
+
+int main()
+{
+    vector <int> divs;
+
+    for (int i = 126849; i <= 126871; i++) {
+        divs.clear();
+        countDel(i, divs);
+
+        if (divs.size() == 4) {
+            sort (divs.begin(), divs.end());
+            for (int i = 0; i < divs.size(); i++) {
+                cout << divs[i] << ' ';
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
