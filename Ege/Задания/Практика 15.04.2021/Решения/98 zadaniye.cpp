5dcd518bea06bc823790571358b7893d9ed45bf6
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-04-17 22:48:04 +0500

    sdf

--- Ege/Задания/Практика 15.04.2021/Решения/98 zadaniye.cpp
+++ Ege/Задания/Практика 15.04.2021/Решения/98 zadaniye.cpp
@@ -1,40 +1,53 @@
 ﻿#include <iostream>
-#include <cmath>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-void findDivs(vector <int>& divs, int n) {
-    for (int i = 2; i * i <= n; i++) {
-        if (i * i == n) {
-            divs.push_back(i);
-        }
-        else if (n % i == 0) {
-            divs.push_back(i);
-            divs.push_back(n / i);
-        }
+void genCubes(vector <int>& cubes) {
+    for (int i = 3; i * i * i <= 531135; i += 2) {
+        cubes.push_back(i * i * i);
     }
 }
 
-int countCubes(vector <int>& divs) {
-    int count = 0;
-    for (int i = 0; i < divs.size(); i++) {
-        if (divs[i] != 0 && int(pow(divs[i], 1. / 3)) * int(pow(divs[i], 1. / 3)) * int(pow(divs[i], 1. / 3)) == divs[i]) {
-            count++;
+bool checkDiv(int n, vector <int> cubes) {
+    for (int i = 0; cubes[i] <= n; i++) {
+        if (cubes[i] == n) {
+            return true;
         }
     }
-    return count;
+    return false;
 }
+
 int main()
 {
-    for (int i = 228224; i <= 531135; i++) {
-        vector <int> divs;
-        findDivs(divs, i);
-        sort(divs.begin(), divs.end());
+    vector <int> cubes;
+    genCubes(cubes);
 
-        if (countCubes(divs) == 4) {
+    for (int i = 250047; i <= 531135; i++) {
+        vector <int> divs;
+
+        for (int j = 2; j * j <= i; j++) {
+            if (j * j == i && checkDiv(j, cubes)) {
+                divs.push_back(j);
+            }
+            else if (i % j == 0) {
+                if (checkDiv(j, cubes)) {
+                    divs.push_back(j);
+                }
+                if (checkDiv(i / j, cubes)) {
+                    divs.push_back(i / j);
+                }
+                
+            }
+        }
+
+        if (divs.size() >= 4) {
+            sort(divs.begin(), divs.end());
+
             cout << i << ' ' << divs.size() << ' ' << divs[divs.size() - 1] << endl;
         }
     }
+
     return 0;
 }
+
