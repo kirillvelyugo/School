300a68badac67af06be292ef9a4f299ee95cdc29
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-04-18 00:07:00 +0500

    sdf

---
+++ Ege/Задания/Практика 17.04.2021/Решения/2 zadaniye.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    map <char, int> s;
+
+    char c;
+    cin >> c;
+
+    while (c != '.') {
+        s[toupper(c)]++;
+        cin >> c;
+    }
+
+    int maxCount = 0;
+    char maxChar = ' ';
+
+    for (char i = 'A'; i <= 'Z'; i++) {
+        if (s[i] > maxCount) {
+            maxCount = s[i];
+            maxChar = i;
+        }
+    }
+
+    cout << maxChar << ' ' << maxCount;
+    return 0;
+}
---
+++ Ege/Задания/Практика 17.04.2021/Решения/3 zadaniye.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+
+int main()
+{
+    map <char, int> chCount;
+    
+    char c;
+    cin >> c;
+
+    while (c != '.') {
+        if (c >= 'a' && c <= 'z'){
+            chCount[c]++;
+        }
+        cin >> c;
+    }
+
+    for (char i = 'a'; i <= 'z'; i++) {
+        if (chCount.count(i) != 0) {
+            cout << i << chCount[i] << endl;
+        }
+    }
+
+    return 0;
+}
