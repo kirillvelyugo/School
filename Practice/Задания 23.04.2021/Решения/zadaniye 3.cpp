c56330166d8c412a279fffae68868f2b1fc98061
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-05-02 00:00:33 +0500

    sdf

---
+++ Practice/Задания 23.04.2021/Решения/zadaniye 3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    pair <int, int> min_count {5001, 0};
+    map <int, pair <int, int> > stat{ {15, min_count}, {20, min_count}, {25, min_count} };
+    int n;
+    cin >> n;
+
+    int minFirst = 5001, minSecond = 5001, minThird = 5001;
+    for (int i = 1; i <= n; i++) {
+        string s;
+        int type, cost;
+        cin >> s >> s >> type >> cost;
+
+        if (stat[type].first > cost) {
+            stat[type].first = cost;
+            stat[type].second = 1;
+        }
+        else if (stat[type].first == cost){
+            stat[type].second++;
+        }
+    }
+
+    cout << stat[15].second << ' ' << stat[20].second << ' ' << stat[25].second << endl;
+
+    return 0;
+}
